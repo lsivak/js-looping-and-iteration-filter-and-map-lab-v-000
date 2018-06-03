906d820e31caae0b80236f05b52760d7acba0694
Lara Sanders <larasanders@gmail.com>
2018-06-03 21:17:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function exactMatch (drivers, attribute) {
 function exactMatchToList (drivers, attribute) {
   return drivers.filter(function (driver) {
     debugger
-    for (const value in attribute) {
+    for (const key of attribute) {
       if(driver.value === attribute);
 return driver.value
     }

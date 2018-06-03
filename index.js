050bc204d9cac5eb2d2162294d97f08fc984b0d7
Lara Sanders <larasanders@gmail.com>
2018-06-03 18:39:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function driversWithRevenueOver (drivers, revenue) {
   debugger
-  return drivers.filter(function (driver)) {
+  return drivers.filter(function (driver) {
     debugger
     return driver.revenue > revenue
   })

f92233f7ae10c0147bc0a3a3cc6ddc76938d2238
Lara Sanders <larasanders@gmail.com>
2018-06-03 19:55:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function driverNamesWithRevenueOver (drivers, revenue) {
 }
 function exactMatch (extendedDrivers, attribute) {
     for(const driver of extendedDrivers) {
-      debugger
-    if (driver.attribute === attribute) {
-      debugger
+      return driver.filter (function (newDriver))
+    if (newDriver.attribute === attribute) {
+      
     return driver
   }
 }

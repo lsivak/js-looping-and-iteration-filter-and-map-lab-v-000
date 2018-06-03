9c3683812b8413fe890d7e446b4b7bed119172de
Lara Sanders <larasanders@gmail.com>
2018-06-03 21:18:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function exactMatchToList (drivers, attribute) {
   return drivers.filter(function (driver) {
     debugger
     for (const key of attribute) {
-      if(driver.attribute=== attribute);
+      if(driver.attribute === attribute);
 return driver.name
     }
   });
